552fbb967b6e73d9a93efb33b607f93daae6170d
Gavryk <gvozdyk23@gmail.com>
2022-07-03 15:24:47 +0300

    added some options for stripe chackout page

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -7,30 +7,49 @@ export default async function handler(req, res) {
         //Create Checkout Session
         try {
             const session = await stripe.checkout.sessions.create({
-                submit_type: 'pay',
-                mode: 'payment',
-                payment_method_types: ['card'],
-                shipping_address_collection: {
-                    allowed_countries: ['US', 'CA']
-                },
-                line_items: req.body.map((item) => {
-                    return {
-                      price_data: {
-                        currency: "usd",
-                        product_data: {
-                          name: item.title,
-                          images: [
-                            item.image.data.attributes.formats.thumbnail.url,
-                          ],
-                        },
-                        unit_amount: item.price * 100,
-                      },
-                      quantity: item.quantity,
-                    };
-                }),
-                //Bring people to the seccess or failed page
-                success_url: `${req.headers.origin}/seccess`,
-                cancel_url: `${req.headers.origin}/canceled`
+              submit_type: "pay",
+              mode: "payment",
+              payment_method_types: ["card"],
+              shipping_address_collection: {
+                allowed_countries: [
+                  "US",
+                  "CA",
+                  "UA",
+                  "PL",
+                  "BG",
+                  "GB",
+                  "FR",
+                  "IT",
+                  "DE",
+                  "PT",
+                ],
+              },
+              allow_promotion_codes: true,
+              shipping_options: [
+                { shipping_rate: "shr_1LHRhMHV0zOLVwJsNXGBb7c7" },
+              ],
+              line_items: req.body.map((item) => {
+                return {
+                  price_data: {
+                    currency: "usd",
+                    product_data: {
+                      name: item.title,
+                      images: [
+                        item.image.data.attributes.formats.thumbnail.url,
+                      ],
+                    },
+                    unit_amount: item.price * 100,
+                  },
+                  adjustable_quantity: {
+                    enabled: true,
+                    minimum: 1
+                  },
+                  quantity: item.quantity,
+                };
+              }),
+              //Bring people to the seccess or failed page
+              success_url: `${req.headers.origin}/seccess`,
+              cancel_url: `${req.headers.origin}/canceled`,
             });
             res.status(200).json(session);
         } catch(error) {
